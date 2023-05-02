df33e884720c1397510f9d1ece17ff971e64fc2b
imJudah <judahjmdasuki@gmail.com>
2023-05-02 13:55:02 +0700

    perbaiki halaman disclaimer

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     ) : (
         <div className='h-screen px-6 text-white flex flex-col gap-4 justify-center'>
             <img src={maintenanceLogo} className='' />
-            <h1 className='text-base font-medium text-center bg-stone-600 p-2 rounded-md'>Situs kami sedang dalam perbaikan. Terima kasih. 🙏🏻</h1>
+            <h1 className='text-base font-medium text-center bg-stone-600 p-2 rounded-md'>Situs kami sedang dalam perbaikan. Silahkan buka di layar besar (tablet, laptop). <br /> Terima kasih 🙏🏻  </h1>
         </div>
     )
 );
