6f3d73b7645d36ecd5b054794b9f2091abe85493
imJudah <judahjmdasuki@gmail.com>
2023-09-26 21:22:56 +0700

    add maintenance section

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,28 @@ const App = () => {
 
     return (
         <>
-            <div className={`relative w-screen h-[${innerHeight}px]`}>
-                <div className="absolute top-0 bottom-0 left-0 right-0">
-                    <Header navButtonOnClick={setNavTextHandle} navText />
-                    <div className="split md:flex">
-                        <main className="w-full h-full overflow-scroll md:relative order-2">
-                            <Routes>
-                                <Route path="/" element={<Home />} />
-                                <Route path="/projects" element={<Projects />} />
-                                <Route path="/contacts" element={<Contacts />} />
-                            </Routes>
-                        </main>
-                        <Footer />
-                        <NavText textContent={navText} />
+            {window.innerWidth > 768 ? (
+                <div className={`relative w-screen h-[${innerHeight}px]`}>
+                    <div className="absolute top-0 bottom-0 left-0 right-0">
+                        <Header navButtonOnClick={setNavTextHandle} navText />
+                        <div className="split md:flex">
+                            <main className="w-full h-full overflow-scroll md:relative order-2">
+                                <Routes>
+                                    <Route path="/" element={<Home />} />
+                                    <Route path="/projects" element={<Projects />} />
+                                    <Route path="/contacts" element={<Contacts />} />
+                                </Routes>
+                            </main>
+                            <Footer />
+                            <NavText textContent={navText} />
+                        </div>
                     </div>
                 </div>
-            </div>
+            ) : (
+                <div className="w-screen h-screen bg-white flex items-center justify-center">
+                    <img src="maintenance-page.png" alt="Under Construction" className='w-full' />
+                </div>
+            )}
         </>
     );
 };
